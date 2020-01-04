82dc7590b346b792fd7c8d05081bb8024fa5c642
Airton Moreira <airton.moreira@gmail.com>
2020-01-04 13:38:42 -0300

    Projeto de comunicação socket - teste finalizado

---
+++ SocketClient/Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is the Cliente");
+            Socket master = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+            master.Connect(ipEnd);
+            //master.Send(System.Text.Encoding.UTF8.GetBytes("Hey"));
+
+            string sendData = "";
+            do
+            {
+                Console.Write("Data to Send:");
+                sendData = Console.ReadLine();
+                master.Send(System.Text.Encoding.UTF8.GetBytes(sendData));
+            } while (sendData.Length > 0);
+            master.Close();
+        }
+    }
+}
---
+++ SocketServer/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the ServerSide");
+
+            Socket listenerSockets = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"),8888);
+            listenerSockets.Bind(ipEnd);
+            listenerSockets.Listen(0);
+            Socket clientSocket = listenerSockets.Accept();
+            byte[] Buffer = new byte[clientSocket.SendBufferSize];
+
+            int readByte;
+
+            do
+            {
+                readByte = clientSocket.Receive(Buffer);
+                byte[] rData = new byte[readByte];
+                Array.Copy(Buffer, rData, readByte);
+                Console.WriteLine("We got: " + System.Text.Encoding.UTF8.GetString(rData));
+            } while (readByte > 0);
+            Console.WriteLine(readByte.ToString());
+            Console.ReadKey();
+                      
+
+        }
+    }
+}
